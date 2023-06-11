42c21bfae2aad5128f1c928b6986397a75d2319a
barizaarfadillah <arfadillah.abqariy@gmail.com>
2023-06-11 16:13:49 +0700

    update

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   if (mongoose.connections.readyState === 1) {
-    console.log("Already connected.");
     return mongoose.connection.asPromise();
   }
   return await mongoose.connect(process.env.MONGODB_URL);
